481e67c1d5817b0594decb33314cea3ef8abf6d4
0BoulderBrother0 <dsamuelsson08@icloud.com>
2026-03-05 18:02:51 +0100

    2026-03-05
    Kan nu skjuta missiler samt finns det meteoriter. Meteoriter har random start velocity och ökar i mängd samt påverkas av missilernas hastighet

---
+++ Assets/Scripts/MeteorScript.cs
@@ -0,0 +1,62 @@
+// using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MeteorScript : MonoBehaviour
+{
+
+    public float HP = 2;
+    public float scale = 3;
+    public float meteorVelocityDecline = 0.1f;
+    public float startVelocity = 2;
+    public float splitSpeed = 2;
+    Vector3 missileVelocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.localScale = new Vector3(scale, scale);
+        HP = scale;
+        GetComponent<Rigidbody2D>().linearVelocity += Random.insideUnitCircle * startVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HP <= 0)
+        {
+            if (scale >= 1.5f)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    GameObject newMeteor = Instantiate(gameObject, transform.position, Quaternion.identity);
+                    Rigidbody2D newMRB = newMeteor.GetComponent<Rigidbody2D>();
+                    MeteorScript newMS = newMeteor.GetComponent<MeteorScript>();
+                    newMS.scale = scale - 1;
+
+                    newMRB.linearVelocity += new Vector2(missileVelocity.x * meteorVelocityDecline, missileVelocity.y * meteorVelocityDecline) + new Vector2(GetComponent<Rigidbody2D>().linearVelocityX, GetComponent<Rigidbody2D>().linearVelocityY);
+                    
+                    if (i == 0)
+                    {
+                        newMRB.linearVelocity += new Vector2(-missileVelocity.y, missileVelocity.x).normalized * splitSpeed;
+                    }
+                    else
+                    {
+                        newMRB.linearVelocity += new Vector2(missileVelocity.y, -missileVelocity.x).normalized * splitSpeed;
+                    }
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Missile"))
+        {
+            missileVelocity = collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity;
+            HP--;
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,14 +1,21 @@
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
 
     Rigidbody2D rb;
+    
     public float thrust = 2;
     public float rotationSpeed = 50;
     public float maxSpeed = 15;
+
+    [Header("Missile")]
+    public GameObject missile;
+    public float shotStrength = 20;
+    public float missileLifeTime = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,11 +35,25 @@ public class PlayerScript : MonoBehaviour
 
         rb.angularVelocity = 0;
 
-        //rb.linearVelocity = Quaternion.Euler(0, 0, rb.rotation) * Vector2.down * rb.linearVelocity.magnitude;
+        // rb.linearVelocity = Quaternion.Euler(0, 0, rb.rotation) * Vector2.down * rb.linearVelocity.magnitude;
 
         if (rb.linearVelocity.magnitude > maxSpeed)
         {
             rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
         }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject newMissile = Instantiate(missile, transform.position + -transform.up * 3, Quaternion.identity);
+            Rigidbody2D mrb = newMissile.GetComponent<Rigidbody2D>();
+            mrb.rotation = rb.rotation + 180;
+            mrb.AddRelativeForceY(shotStrength, ForceMode2D.Impulse);
+            Destroy(newMissile, missileLifeTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
