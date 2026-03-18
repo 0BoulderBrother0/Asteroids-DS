be33829c865bad1096c24a0310bc5c3bf06b730b
Abdulrahman Al-Walid <ac73102@staden.gotheborg.net>
2026-03-18 09:19:49 +0100

    rahman
    fixat så att man kan byta låt mellan två olika låter med knappen M

--- Assets/Scripts/MusicManagerScript.cs
+++ Assets/Scripts/MusicManagerScript.cs
@@ -4,6 +4,11 @@ public class MusicManagerScript : MonoBehaviour
 {
 
     public static MusicManagerScript instance;
+    public AudioSource audioSource;
+    public AudioClip ljud1;
+    public AudioClip ljud2;
+    public bool isSong1Playing = true;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,12 +22,27 @@ public class MusicManagerScript : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = ljud1;
+        audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (isSong1Playing)
+            {
+                audioSource.clip = ljud1;
+            }
+            else
+            {
+                audioSource.clip = ljud2;
+            }
+
+            audioSource.Play();
+            isSong1Playing = !isSong1Playing;
+        }
     }
 }
